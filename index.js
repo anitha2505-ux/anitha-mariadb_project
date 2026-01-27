c2a5915ee8ef1eb6b7fc6680e7879f8d700cf3a5
anitha2505-ux <anitha2505@gmail.com>
2026-01-27 07:38:05 +0000

    UI First Iteration with Select All

---
+++ index.js
@@ -0,0 +1,123 @@
+const express = require("express");
+// ejs is embedded JavaScript (is a way to create templates for a dynamic web app)
+// a template file is a reusable HTML code which express can
+// send back to the client
+const ejs = require("ejs");
+const expressLayouts = require("express-ejs-layouts");
+// mysql2 is a NodeJS client to do CRUD with MySQL/MariaDB
+const mysql2 = require("mysql2/promise");
+require("dotenv").config();
+
+const app = express();
+const port = 3000;
+
+//setup Flash
+const session = require("express-session");
+const flash = require("connect-flash");
+const { createPool } = require("mysql2");
+
+// setup EJS
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+//layout setup 
+app.use(expressLayouts);
+app.set("layout", "03_layout");
+
+// enables form processing on the server side
+app.use(express.urlencoded({
+    extended: true
+}))
+
+// enables flash messages on session
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+    })
+)
+
+app.use(flash());
+
+//make flash messages available for EJS pages
+
+app.use((req, res, next) => {
+    res.locals.messages = req.flash();
+    next();
+});
+
+// define a global title once 
+app.use((req, res, next) => {
+    res.locals.title = "PawfectCare";
+    next();
+});
+
+// create a connection pool
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}
+
+const dbConnection = mysql2.createPool(dbConfig);
+// home page view
+
+app.get("/PawfectCare", function (req, res) {
+    res.render('01_home')
+})
+
+// Read from bookings table
+app.get("/bookings", async function (req, res) {
+    const sql = 'SELECT * FROM bookings'
+    const results = await dbConnection.query(sql);
+    const rows = results[0];
+
+    res.render('05_bookings_index', {
+        bookings: rows
+    })
+})
+
+// Read from services table
+app.get("/services", async function (req, res) {
+    const sql = 'SELECT * FROM services'
+    const results = await dbConnection.query(sql);
+    const rows = results[0];
+
+    res.render('11_services_index', {
+        services: rows
+    })
+})
+
+// Read from pets table
+app.get("/pets", async function (req, res) {
+    const sql = 'SELECT * FROM pets'
+    const results = await dbConnection.query(sql);
+    const rows = results[0];
+
+    res.render('10_pets_index', {
+        pets: rows
+    })
+})
+
+// read from owners table
+app.get("/owners", async function (req, res) {
+    const sql = 'SELECT * FROM owners'
+    const results = await dbConnection.query(sql);
+    const rows = results[0];
+
+    res.render('09_owners_index', {
+        owners: rows
+    })
+})
+
+app.get("/bookings/new", function (req, res) {
+    res.render("06_bookings_new");
+})
+
+
+app.listen(3000, () => {
+    console.log('server is running');
+})
---
+++ schema.sql
@@ -0,0 +1,233 @@
+-- Create database pawfectcare
+CREATE DATABASE pawfectcare;
+
+-- use database pawfectcare
+USE pawfectcare;
+
+-- show databases
+SHOW DATABASES;
+
+-- create table owners
+CREATE TABLE owners (
+-- <column name> <data type> <options>
+    ownerId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(255),
+    lastName VARCHAR(255),
+    email VARCHAR(100),
+    phone VARCHAR(20)
+) engine = innodb;
+
+-- create table pets
+CREATE TABLE pets(
+-- <column name> <data type> <options>
+    petId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    petName VARCHAR(50),
+    species VARCHAR(30),
+    dateOfBirth DATE
+) engine = innodb;
+
+-- create table bookings
+CREATE TABLE bookings (
+-- <column name> <data type> <options>
+    bookingId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    bookingDate DATE,
+    startTime TIME,
+    endTime TIME,
+    durationMinutes TINYINT UNSIGNED,
+    description TEXT
+) engine = innodb;
+
+-- create table services
+CREATE TABLE services (
+-- <column name> <data type> <options>
+    serviceId INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    serviceName VARCHAR(80),
+    serviceType VARCHAR(30),
+    price DECIMAL,
+    durationMinutes TINYINT UNSIGNED,
+    description TEXT        
+) engine = innodb;
+
+-- to show tables 
+SHOW TABLES;
+
+-- ALTER TABLE BOOKINGS TO REMOVE WRONGLY CREATED COLUMNS
+ALTER TABLE bookings
+DROP COLUMN durationMinutes,
+DROP COLUMN description;
+
+-- to describe table details 
+DESCRIBE bookings;
+
+-- assign Foreign Keys to tables
+-- set owner.ownerId as foreign key in pets Table
+ALTER TABLE pets
+ADD COLUMN ownerId INT UNSIGNED;
+
+ALTER TABLE pets 
+ADD CONSTRAINT fk_pets_owner
+FOREIGN KEY (ownerId)
+REFERENCES owners(ownerId)
+ON UPDATE CASCADE
+ON DELETE RESTRICT;
+
+-- set owner.ownerId, pets.petId as foreign keys in bookings Table
+ALTER TABLE bookings
+ADD COLUMN ownerId INT UNSIGNED,
+ADD COLUMN petId INT UNSIGNED;
+
+ALTER TABLE bookings
+ADD CONSTRAINT fk_bookings_owners
+FOREIGN KEY (ownerId)
+REFERENCES owners(ownerId)
+ON UPDATE CASCADE
+ON DELETE RESTRICT;
+
+ALTER TABLE bookings
+ADD CONSTRAINT fk_bookings_pets
+FOREIGN KEY (petId)
+REFERENCES pets(petId)
+ON UPDATE CASCADE
+ON DELETE RESTRICT;
+
+-- to modify columns data type options to Not Null for the below 
+ALTER TABLE owners
+MODIFY firstName VARCHAR(255) NOT NULL,
+MODIFY lastName VARCHAR(255) NOT NULL,
+MODIFY email VARCHAR(100) NOT NULL,
+MODIFY phone VARCHAR(20) NOT NULL;
+
+ALTER TABLE pets
+MODIFY petName VARCHAR(50) NOT NULL,
+MODIFY species VARCHAR(30) NOT NULL,
+MODIFY ownerId INT UNSIGNED NOT NULL;
+
+ALTER TABLE services
+MODIFY serviceName VARCHAR(80) NOT NULL,
+MODIFY serviceType VARCHAR(30) NOT NULL,
+MODIFY price DECIMAL(10,2) NOT NULL,
+MODIFY durationMinutes TINYINT UNSIGNED NOT NULL;
+
+ALTER TABLE bookings
+MODIFY bookingDate DATE NOT NULL,
+MODIFY startTime TIME NOT NULL,
+MODIFY endTime TIME NOT NULL,
+ADD COLUMN status VARCHAR(20) NOT NULL,
+MODIFY ownerId INT UNSIGNED NOT NULL,
+MODIFY petID INT UNSIGNED NOT NULL;
+
+-- to change petID in bookings to petId when there is foreign key
+ALTER TABLE bookings
+DROP FOREIGN KEY fk_bookings_pets,
+CHANGE COLUMN petID petId INT UNSIGNED NOT NULL,
+ADD CONSTRAINT fk_bookings_pets
+FOREIGN KEY (petId)
+REFERENCES pets(petId)
+ON UPDATE CASCADE
+ON DELETE RESTRICT;
+
+-- 
+-- insert sample data to tables - 1 record each
+INSERT INTO owners (firstName, lastName, email, phone) VALUES ("Anitha", "Gayathri", "anitha@123.com", "12345678");
+INSERT INTO pets(petName, species, dateOfBirth, ownerId) VALUES ("Benny", "Dog", "2015-01-01", 1);
+INSERT INTO services(serviceName, serviceType, price, durationMinutes, description) VALUES ("Basic Grooming","Grooming",100.00, 60, "Bathing, brushing, and nail trimming");
+INSERT INTO bookings(bookingDate, startTime, endTime, ownerId, petId, status) VALUES ('2026-02-10', '10:00:00', '11:00:00', 1, 1, 'Pending');
+
+-- insert sample date to tables - 4 records each at the same time 
+INSERT INTO owners (firstName, lastName, email, phone) VALUES
+('Ravi', 'Kumar', 'ravi.kumar@email.com', '91234567'),
+('Mei', 'Ling', 'meiling@email.com', '81234567'),
+('Daniel', 'Tan', 'daniel.tan@email.com', '98765432'),
+('Siti', 'Aminah', 'siti.aminah@email.com', '93456789');
+
+INSERT INTO pets (petName, species, dateOfBirth, ownerId) VALUES
+('Max', 'Dog', '2018-06-15', 2),
+('Luna', 'Cat', '2020-03-10', 3),
+('Charlie', 'Dog', '2017-11-25', 4),
+('Milo', 'Cat', '2019-08-05', 5);
+
+INSERT INTO services (serviceName, serviceType, price, durationMinutes, description) VALUES
+('Full Grooming', 'Grooming', 150.00, 90, 'Full grooming including haircut and ear cleaning'),
+('Pet Walking 30 Min', 'Walking', 25.00, 30, '30-minute outdoor walking session'),
+('Pet Boarding (Day)', 'Boarding', 60.00, 240, 'Daytime boarding with feeding and supervision'),
+('Basic Training', 'Training', 120.00, 60, 'Basic obedience training session');
+
+INSERT INTO bookings (bookingDate, startTime, endTime, ownerId, petId, status) VALUES
+('2026-02-11', '09:00:00', '10:30:00', 2, 2, 'Confirmed'),
+('2026-02-12', '14:00:00', '15:00:00', 3, 3, 'Pending'),
+('2026-02-13', '16:00:00', '17:00:00', 4, 4, 'Completed'),
+('2026-02-14', '11:00:00', '12:00:00', 5, 5, 'Confirmed');
+
+-- change datatype of services.durationMinutes to smallINT unsigned from tintINT unsigned
+ALTER TABLE services
+MODIFY durationMinutes SMALLINT UNSIGNED NOT NULL;
+
+-- checking update of small int in services 
+SELECT serviceId, serviceName, durationMinutes
+FROM services
+ORDER BY serviceId DESC
+LIMIT 1;
+
+-- create Join Table - bookingServices
+CREATE TABLE bookingServices (
+  bookingServiceId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  bookingId INT UNSIGNED NOT NULL,
+  serviceId INT UNSIGNED NOT NULL,
+  CONSTRAINT fk_bookingServices_bookings
+    FOREIGN KEY (bookingId)
+    REFERENCES bookings(bookingId)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT,
+  CONSTRAINT fk_bookingServices_service
+    FOREIGN KEY (serviceId)
+    REFERENCES services(serviceId)
+    ON UPDATE CASCADE
+    ON DELETE RESTRICT
+) ENGINE=InnoDB;
+
+-- to view meaningful columns in Bookings Table
+SELECT 
+        b.bookingId,
+        b.bookingDate,
+        b.startTime,
+        b.endTime,
+        b.status,
+
+        o.ownerId,
+        o.firstName,
+        o.lastName,
+        o.email,
+        o.phone,
+
+        p.petId,
+        p.petName,
+        p.species,
+
+        COALESCE(
+          GROUP_CONCAT(DISTINCT s.serviceName ORDER BY s.serviceName SEPARATOR ', '),
+          'No Services'
+        ) AS servicesBooked
+
+      FROM bookings b
+      JOIN owners o ON b.ownerId = o.ownerId
+      JOIN pets p ON b.petId = p.petId
+      LEFT JOIN bookingServices bs ON b.bookingId = bs.bookingId
+      LEFT JOIN services s ON bs.serviceId = s.serviceId
+
+      GROUP BY 
+        b.bookingId,
+        b.bookingDate,
+        b.startTime,
+        b.endTime,
+        b.status,
+        o.ownerId,
+        o.firstName,
+        o.lastName,
+        o.email,
+        o.phone,
+        p.petId,
+        p.petName,
+        p.species
+
+      ORDER BY b.bookingId DESC;
+    
